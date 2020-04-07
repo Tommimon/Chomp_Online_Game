171efca364b6ae1e5bb3a580d9747e00a5e35863
Tommimon <37435103+Tommimon@users.noreply.github.com>
2020-04-07 18:42:20 +0200

    aggiunti file py iniziali

---
+++ client.py
@@ -0,0 +1,34 @@
+import pygame
+from tavola import *
+succes, fail = pygame.init()
+print("{0} successes and {1} failures".format(succes, fail))
+
+# PARAMETRI
+FPS = 60  # Frames per second.
+RISOLUZIONE = (720, 480)
+NERO = (0, 0, 0)
+DIM_CAS = 64
+PADDING = 4
+
+
+screen = pygame.display.set_mode(RISOLUZIONE)  # mostro schermo
+clock = pygame.time.Clock()  # inizializzo clock
+
+tavoletta = Tavola(0, 0, 720, 480, 10, 20, PADDING)
+tavoletta.disegna()
+
+while True:
+    clock.tick(FPS)  # mi fa andare al giusto frame rate
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                tavoletta.ceck_click(event.pos)
+        elif event.type == FINEPARTITA:
+            print('fine' + event.win)
+
+    screen.fill(NERO)
+    tavoletta.blit(screen)
+    pygame.display.update()  # Or pygame.display.flip()
